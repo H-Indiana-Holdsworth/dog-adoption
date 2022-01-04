bbb68073c57fb91c5669bd5c3fca42ef23a0f70f
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2022-01-04 15:51:50 -0700

    Adds loading state to DogCard

--- src/Views/DogCard.js
+++ src/Views/DogCard.js
@@ -5,15 +5,21 @@ import DogDetail from '../Components/DogDetail';
 
 export default function DogCard(props) {
   const [dogCard, setDogCard] = useState([]);
+  const [loading, setLoading] = useState(true);
   const id = props.match.params.id;
 
   useEffect(() => {
     const fetchData = async () => {
       const resp = await getDogsById(id);
       setDogCard(resp);
+      setLoading(false);
     };
     fetchData();
   }, [id]);
+
+  if (loading) {
+    return <h3>Loading...</h3>;
+  }
 
   return (
     <div>

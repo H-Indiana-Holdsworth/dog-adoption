b44f071290742fcec0c15a3315c980579d7e002c
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2022-01-07 19:33:22 -0700

    Test fix

--- src/Views/Dogs.test.js
+++ src/Views/Dogs.test.js
@@ -5,15 +5,6 @@ import Dogs from './Dogs';
 test('should render Dogs component', async () => {
   const { container } = render(
     <MemoryRouter>
-      {/* <Dogs
-        dogs={{
-          id: 2,
-          name: 'Myra',
-          bio: 'Shy.',
-          breed: 'Shepard mix',
-          image: 'https://placedog.net/500?id=73',
-        }}
-      /> */}
       <Dogs
         dogs={{
           header: 'Meet Myra',
